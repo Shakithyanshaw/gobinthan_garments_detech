c8a4a93b8ed330ea80fba6de20b5cdbc7840a934
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2025-02-19 14:55:04 +0530

    show data in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import data from './data';
+
 function App() {
   return (
-    <div className="App">
+    <div>
       <header>
         <a href="/">Gobinthan Garments</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>Feature Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p> {product.name}</p>
+                </a>
+                <p>
+                  {' '}
+                  <strong>Code:-{product.code}</strong>
+                </p>
+                <button>Add to card</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
