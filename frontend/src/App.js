cf71aa922695e7e7e23add854533b2959a3ea210
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2025-02-20 19:02:52 +0530

    code sign in screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Nav from 'react-bootstrap/esm/Nav';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CardScreen from './screens/CardScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -45,6 +46,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/" element={<HomeScreen />} />
               <Route path="/cart" element={<CardScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
             </Routes>
           </Container>
         </main>
