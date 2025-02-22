c3fedc12770aa0ff4d33e263639f4a948efdb94b
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2025-02-22 13:12:29 +0530

    create product model & route

---
+++ backend/models/productModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    code: { type: String, required: true, unique: true }, // Added code field
+    slug: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    colour: { type: String, required: true }, // Added colour field
+    size: { type: String, required: true }, // Added size field
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    countInStock: { type: Number, required: true },
+    brand: { type: String, required: true },
+    rating: { type: Number, required: true },
+    numReviews: { type: Number, required: true },
+    description: { type: String, required: true },
+    fabric: { type: String, required: true }, // Added fabric field
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
