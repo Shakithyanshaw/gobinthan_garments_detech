e8c33e863e8b343c9e57948fe4c9c7e6131a6e10
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2025-02-23 12:15:37 +0530

    code signin action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Badge from 'react-bootstrap/esm/Badge';
 import Nav from 'react-bootstrap/esm/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CardScreen from './screens/CardScreen';
 import SigninScreen from './screens/SigninScreen';
 
 function App() {
-  const { state } = useContext(Store);
-  const { cart } = state;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+  };
+
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1} />
         <header>
           <Navbar
             style={{ backgroundColor: '#801001', height: '167px' }}
@@ -36,6 +46,33 @@ function App() {
                     </Badge>
                   )}
                 </Link>
+                {userInfo ? (
+                  <NavDropdown
+                    title={userInfo.name}
+                    id="collasible-nav-dropdown"
+                  >
+                    <NavDropdown.Item as={Link} to="/profile">
+                      <span className="text-dark">User Profile</span>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item as={Link} to="/orderhistory">
+                      <span className="text-dark">Order History</span>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      <span className="text-dark">Signout</span>
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    <span className="text-light">Signin</span>
+                  </Link>
+                )}
               </Nav>
             </Container>
           </Navbar>
